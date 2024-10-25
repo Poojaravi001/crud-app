b7b80aee5342ec697a0cae1b09d013a939462f18
poojaravi <poojaravi0533@gmail.com>
2024-10-25 12:02:42 +0530

    ok

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,22 @@ Route:: get("products/{id}/show",[ProductController::class,'show']);
 
 Route:: get("products/{id}/edit",[ProductController::class,'edit']);
 
-Route:: put("products/{id}/update",[ProductController::class,'update'])->name('products.update');
+// Delete Product
+Route::delete("products/{id}/destroy", [ProductController::class, 'destroy'])->name('products.destroy');
 
-Route:: get("products/{id}/delete",[ProductController::class,'destroy']);
+// Delete Product Image
+Route::delete("products/{product}/images/{image}", [ProductController::class, 'destroyImage'])->name('products.image.destroy');
+
+// Purchase Routes
+Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+Route::get('/purchases/create', [PurchaseController::class, 'create'])->name('purchases.create');
+Route::post('/purchases', [PurchaseController::class, 'store'])->name('purchases.store'); // Use this for submission
+// List All Purchases
+Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+
+// Create Purchase Form
+Route::get('/purchases/create', [PurchaseController::class, 'create'])->name('purchases.create');
+
+// Store Purchase Data
+Route::post('/purchases', [PurchaseController::class, 'store'])->name('purchases.store');
+
