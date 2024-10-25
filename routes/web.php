fc9047843c50b533e0b40b87c1a2c9e62137ccf7
nandhinikalai <nandhini.rajendran0412@gmail.com>
2024-10-25 11:41:52 +0100

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,27 @@
 <?php
 
-use App\Http\Controllers\productController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SalesItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route:: get("/",[ProductController::class,'index']);
+Route::get("/", [ProductController::class, 'index']);
+Route::get("products/create", [ProductController::class, 'create']);
+Route::post("products/store", [ProductController::class, 'store'])->name('products.store');
+Route::get("products/{id}/show", [ProductController::class, 'show']);
+Route::get("products/{id}/edit", [ProductController::class, 'edit']);
+Route::put("products/{id}/update", [ProductController::class, 'update'])->name('products.update');
+Route::get("products/{id}/delete", [ProductController::class, 'destroy']);
 
-Route:: get("products/create",[ProductController::class,'create']);
+//  route for customers
+Route::get('customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::post('/customers/store', [CustomerController::class, 'store'])->name('customers.store');
+Route::get('/sales/create', [CustomerController::class, 'create'])->name('sales.create');
+Route::post('/sales', [CustomerController::class, 'store'])->name('sales.store');
+Route::post('/sales/items', [SalesItemController::class, 'store'])->name('sales.items.store');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+Route::get('/product-details/{id}', [ProductController::class, 'getProductDetails']);
 
-Route:: post("products/store",[ProductController::class,'store'])->name('products.store');
 
-Route:: get("products/{id}/show",[ProductController::class,'show']);
 
-Route:: get("products/{id}/edit",[ProductController::class,'edit']);
-
-Route:: put("products/{id}/update",[ProductController::class,'update'])->name('products.update');
-
-Route:: get("products/{id}/delete",[ProductController::class,'destroy']);
